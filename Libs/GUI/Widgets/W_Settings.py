44aa45c9ad41784b3872536d9f731afe6390f0ca
Jan Vaško <50368892+JanVasko1@users.noreply.github.com>
2025-03-05 23:40:04 +0100

    General Updates 13

--- Libs/GUI/Widgets/W_Settings.py
+++ Libs/GUI/Widgets/W_Settings.py
@@ -169,21 +169,21 @@ def Settings_User_Access(Settings: dict, Configuration: dict, Frame: CTk|CTkFram
     # Field - Display Name
     NAV_Display_name_Frame = Elements_Groups.Get_Widget_Input_row(Settings=Settings, Configuration=Configuration, Frame=Frame_Body, Field_Frame_Type="Single_Column" , Label="Client Name", Field_Type="Input_Normal") 
     NAV_Display_name_Frame_Var = NAV_Display_name_Frame.children["!ctkframe3"].children["!ctkentry"]
-    NAV_Display_name_Frame_Var.configure(placeholder_text="Enter Name of Auth app", placeholder_text_color="#949A9F")
+    NAV_Display_name_Frame_Var.configure(placeholder_text="Enter Name of Auth app")
     NAV_Display_name_Frame_Var.bind("<FocusOut>", lambda Entry_value: Defaults_Lists.Save_set_key_env(Key="Display_name", Value=NAV_Display_name_Frame_Var.get()))
     Entry_field_Insert(Field=NAV_Display_name_Frame_Var, Value=Display_name)
 
     # Field - Client ID
     NAV_Client_ID_Frame = Elements_Groups.Get_Widget_Input_row(Settings=Settings, Configuration=Configuration, Frame=Frame_Body, Field_Frame_Type="Single_Column" , Label="Client ID", Field_Type="Input_Normal") 
     NAV_Client_ID_Frame_Var = NAV_Client_ID_Frame.children["!ctkframe3"].children["!ctkentry"]
-    NAV_Client_ID_Frame_Var.configure(placeholder_text="Enter Client ID of Auth app", placeholder_text_color="#949A9F")
+    NAV_Client_ID_Frame_Var.configure(placeholder_text="Enter Client ID of Auth app")
     NAV_Client_ID_Frame_Var.bind("<FocusOut>", lambda Entry_value: Defaults_Lists.Save_set_key_env(Key="client_id", Value=NAV_Client_ID_Frame_Var.get()))
     Entry_field_Insert(Field=NAV_Client_ID_Frame_Var, Value=client_id)
 
     # Field - Client Secret
     NAV_Client_Secret_Frame = Elements_Groups.Get_Widget_Input_row(Settings=Settings, Configuration=Configuration, Frame=Frame_Body, Field_Frame_Type="Single_Column" , Label="Client Secret", Field_Type="Password_Normal")
     NAV_Client_Secret_Frame_Var = NAV_Client_Secret_Frame.children["!ctkframe3"].children["!ctkentry"]
-    NAV_Client_Secret_Frame_Var.configure(placeholder_text="Enter Secret ID of Auth app", placeholder_text_color="#949A9F")
+    NAV_Client_Secret_Frame_Var.configure(placeholder_text="Enter Secret ID of Auth app")
     NAV_Client_Secret_Frame_Var.bind("<FocusOut>", lambda Entry_value: Defaults_Lists.Save_set_key_env(Key="client_secret", Value=NAV_Client_Secret_Frame_Var.get()))
     Entry_field_Insert(Field=NAV_Client_Secret_Frame_Var, Value=client_secret)
 
